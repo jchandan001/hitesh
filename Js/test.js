1233c097bb9fd340919e696eda38d69ac816ede4
jchandan001 <120767441+jchandan001@users.noreply.github.com>
2023-09-19 05:14:58 +0000

    object learning

--- Js/test.js
+++ Js/test.js
@@ -1,1 +1,29 @@
 console.log('Chandan');
+
+
+const customerId = 3344; // Can't re-assign value to Constant
+
+let customerName = "Deepak"
+var customerCity = "Jaipur"
+// console.log(customerCity);
+// console.log(customerName);
+// console.log(typeof "Chandan")
+
+// Conversion
+
+let age = 44;
+
+let ageString = String(age);
+
+console.log(typeof ageString);
+
+// Stiring
+
+let schoolName = "B.N.Se.Sec.School"
+console.log(typeof schoolName);
+
+
+let myName = new String("Chandan");
+console.log(typeof myName)
+
+
